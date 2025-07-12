65586ac1f39903c7e8d19e9abd840620d9a9039a
Gabriel Baptista Cerqueira <66979159+gbcbaptista@users.noreply.github.com>
2025-07-12 10:57:56 -0300

    fix: backend url

--- ui/src/requests.ts
+++ ui/src/requests.ts
@@ -1,6 +1,12 @@
 const requests = {
   classifyDog: async (formData: FormData) => {
-    return await fetch("/api/classify", {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+    if (!apiUrl) {
+      throw new Error("API URL not configured in environment variables.");
+    }
+
+    return await fetch(apiUrl, {
       method: "POST",
       body: formData,
     });
