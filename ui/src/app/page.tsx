209d80b5842fc812fa8c2dae2987858f47522ba0
Gabriel Baptista Cerqueira <66979159+gbcbaptista@users.noreply.github.com>
2025-07-20 22:08:35 -0300

    feat: adding supported breeds to the home page

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -15,6 +15,9 @@ export default function Home() {
           {!result ? <ImageUpload /> : <ResultDisplay />}
         </div>
       </section>
+      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+        <SupportedBreeds />
+      </div>
       <section className="pt-12 lg:pt-20 w-full">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
           <Tips />
